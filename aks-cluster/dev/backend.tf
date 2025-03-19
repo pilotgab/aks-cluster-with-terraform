77d4fac52119048929e048f6e9061679b183c05f
pilotgab <captain.gab@protonmail.com>
2025-03-19 17:29:54 +0100

    udate

--- aks-cluster/dev/backend.tf
+++ aks-cluster/dev/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "terraform-state-rg"
-    storage_account_name = "tfdevbackend2024piyush"
+    storage_account_name = "pilotgabdevbackend2025"
     container_name      = "tfstate"
-    key                 = "dev.tfstate"
+    key                 = "dev.terraform.tfstate"
   }
 }
 
--- aks-cluster/staging/backend.tf
+++ aks-cluster/staging/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "terraform-state-rg"
-    storage_account_name = "tfstagebackend2024piyush"
+    storage_account_name = "pilotgabstagebackend2025"
     container_name      = "tfstate"
-    key                 = "stage.tfstate"
+    key                 = "stage.terraform.tfstate"
   }
 }
 
