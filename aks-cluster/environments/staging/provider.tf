ef92e9f2242ad5298a40dda82523a374e6d62cac
pilotgab <captain.gab@protonmail.com>
2025-03-20 02:05:38 +0100

    update

--- aks-cluster/environments/staging/provider.tf
+++ aks-cluster/environments/staging/provider.tf
@@ -27,5 +27,13 @@ provider "azurerm" {
   }
 }
 provider "azuread" {
-  
+
+}
+
+provider "azurerm" {
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
 }
