1a3251fa5980b3c7bd879b1b2603170861ce188f
pilotgab <captain.gab@protonmail.com>
2025-03-20 02:10:56 +0100

    update

--- aks-cluster/environments/staging/provider.tf
+++ aks-cluster/environments/staging/provider.tf
@@ -1,8 +1,3 @@
-# Configure the Azure provider, you can have many
-# if you use azurerm provider, it's source is hashicorp/azurerm
-# short for registry.terraform.io/hashicorp/azurerm
-
-
 terraform {
   required_providers {
     azurerm = {
@@ -14,26 +9,18 @@ terraform {
       version = "~> 3.0.2"
     }
   }
-
   required_version = ">= 1.9.0"
 }
-# configures the provider
 
 provider "azurerm" {
   features {
     key_vault {
       purge_soft_delete_on_destroy = false
     }
-  }
-}
-provider "azuread" {
-
-}
-
-provider "azurerm" {
-  features {
     resource_group {
       prevent_deletion_if_contains_resources = false
     }
   }
 }
+
+provider "azuread" {}
