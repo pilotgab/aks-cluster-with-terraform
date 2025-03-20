349ae0b75c9f45433fab29cbcbb21163dd591636
pilotgab <captain.gab@protonmail.com>
2025-03-20 19:28:38 +0100

    update

--- aks-cluster/environments/dev/main.tf
+++ aks-cluster/environments/dev/main.tf
@@ -1,6 +1,17 @@
+# Root main.tf - Clean and fully automated version
+
 resource "azurerm_resource_group" "rg1" {
   name     = var.rgname
   location = var.location
+}
+
+resource "azurerm_log_analytics_workspace" "this" {
+  name                = "${var.name}-law"
+  location            = var.location
+  resource_group_name = var.rgname
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
+  tags                = var.tags
 }
 
 module "ServicePrincipal" {
@@ -13,7 +24,6 @@ module "ServicePrincipal" {
 }
 
 resource "azurerm_role_assignment" "rolespn" {
-
   scope                = "/subscriptions/${var.SUB_ID}"
   role_definition_name = "Contributor"
   principal_id         = module.ServicePrincipal.service_principal_object_id
@@ -47,7 +57,23 @@ resource "azurerm_key_vault_secret" "pilotgab_kv" {
   ]
 }
 
-#create Azure Kubernetes Service
+module "vnet" {
+  source                      = "../../modules/vnet/"
+  name                        = var.name
+  resource_group_name         = var.rgname
+  location                    = var.location
+  address_space               = var.address_space
+  public_subnet_cidrs         = var.public_subnet_cidrs
+  private_subnet_cidrs        = var.private_subnet_cidrs
+  firewall_subnet_cidr        = var.firewall_subnet_cidr
+  log_analytics_workspace_id  = azurerm_log_analytics_workspace.this.id
+
+  depends_on = [
+    azurerm_resource_group.rg1,
+    azurerm_log_analytics_workspace.this
+  ]
+}
+
 module "aks" {
   source                 = "../../modules/aks/"
   service_principal_name = var.service_principal_name
@@ -64,28 +90,10 @@ module "aks" {
     module.ServicePrincipal,
     module.vnet
   ]
-
 }
 
 resource "local_file" "kubeconfig" {
-  depends_on   = [module.aks]
-  filename     = "./kubeconfig"
-  content      = module.aks.config
-
-}
-
-
-module "vnet" {
-  source                      = "../../modules/vnet"
-  name                        = var.name
-  resource_group_name         = var.rgname
-  location                    = var.location
-  address_space               = var.address_space
-  public_subnet_cidrs         = var.public_subnet_cidrs
-  private_subnet_cidrs        = var.private_subnet_cidrs
-  firewall_subnet_cidr        = var.firewall_subnet_cidr
-  log_analytics_workspace_id  = var.log_analytics_workspace_id
-
-
-  depends_on = [azurerm_resource_group.rg1]
+  depends_on = [module.aks]
+  filename   = "./kubeconfig"
+  content    = module.aks.config
 }
