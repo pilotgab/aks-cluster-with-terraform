b9be3405db68ef01bf13e84d8e029f9e1108517c
pilotgab <captain.gab@protonmail.com>
2025-03-22 14:43:08 +0100

    update

--- aks-cluster/modules/aks/main.tf
+++ aks-cluster/modules/aks/main.tf
@@ -22,7 +22,7 @@ resource "azurerm_kubernetes_cluster" "aks-cluster" {
     vm_size    = "Standard_DS2_v2"
     zones   = [1, 2, 3]
     auto_scaling_enabled = true
-    max_count            = 2
+    max_count            = 3
     min_count            = 2
     os_disk_size_gb      = 30
     type                 = "VirtualMachineScaleSets"
