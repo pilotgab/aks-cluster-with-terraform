d2f83db7b8f4da2c298ff5d802b82119ccfbb530
pilotgab <captain.gab@protonmail.com>
2025-03-24 21:04:23 +0100

    update

--- aks-cluster/modules/vnet/main.tf
+++ aks-cluster/modules/vnet/main.tf
@@ -118,7 +118,20 @@ resource "azurerm_network_security_group" "public" {
     access                     = "Allow"
     protocol                   = "Tcp"
     source_port_range          = "*"
-    destination_port_range     = ["80", "443"]
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+
+  security_rule {
+    name                       = "AllowInternetInbound"
+    priority                   = 101
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "443"
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
