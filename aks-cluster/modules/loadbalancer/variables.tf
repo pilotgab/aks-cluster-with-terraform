7129f085c92254657ac1f9af0d3391c3578eedf8
pilotgab <captain.gab@protonmail.com>
2025-03-26 20:50:43 +0100

    update

---
+++ aks-cluster/modules/loadbalancer/variables.tf
@@ -0,0 +1,14 @@
+variable "resource_group_name" {
+  description = "Resource group name"
+  type        = string
+}
+
+variable "location" {
+  description = "Azure region"
+  type        = string
+}
+
+variable "cluster_name" {
+  description = "AKS cluster name"
+  type        = string
+}
