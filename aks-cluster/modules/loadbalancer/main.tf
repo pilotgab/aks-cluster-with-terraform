66a770cec0fecf883fa0339af77b10b2682b4590
pilotgab <captain.gab@protonmail.com>
2025-03-28 15:30:40 +0100

    update

--- aks-cluster/modules/loadbalancer/main.tf
+++ aks-cluster/modules/loadbalancer/main.tf
@@ -1,17 +1,17 @@
-
+# Public IP for Load Balancer
 resource "azurerm_public_ip" "aks_lb_public_ip" {
   name                = "${var.cluster_name}-custom-lb-pip"
   location            = var.location
-  resource_group_name = var.resource_group_name
+  resource_group_name = var.resource_group_name # "stage-pilotgab-rg"
   allocation_method   = "Static"
   sku                 = "Standard"
 }
 
-
+# Load Balancer
 resource "azurerm_lb" "aks_custom_lb" {
   name                = "${var.cluster_name}-custom-lb"
   location            = var.location
-  resource_group_name = var.resource_group_name
+  resource_group_name = var.resource_group_name # "stage-pilotgab-rg"
   sku                 = "Standard"
 
   frontend_ip_configuration {
@@ -20,13 +20,13 @@ resource "azurerm_lb" "aks_custom_lb" {
   }
 }
 
-# Backend Address Pool (correct)
+# Backend Address Pool
 resource "azurerm_lb_backend_address_pool" "aks_backend_pool" {
   name            = "aks-backend-pool"
   loadbalancer_id = azurerm_lb.aks_custom_lb.id
 }
 
-
+# Health Probe
 resource "azurerm_lb_probe" "aks_http_probe" {
   name                = "http-health-probe"
   loadbalancer_id     = azurerm_lb.aks_custom_lb.id
@@ -37,6 +37,7 @@ resource "azurerm_lb_probe" "aks_http_probe" {
   number_of_probes    = 2
 }
 
+# Load Balancing Rule
 resource "azurerm_lb_rule" "aks_http_rule" {
   name                           = "http-rule"
   loadbalancer_id                = azurerm_lb.aks_custom_lb.id
@@ -48,22 +49,17 @@ resource "azurerm_lb_rule" "aks_http_rule" {
   probe_id                       = azurerm_lb_probe.aks_http_probe.id
 }
 
-data "azurerm_kubernetes_cluster" "main" {
-  name                = var.cluster_name
-  resource_group_name = var.aks_resource_group_name
-}
-
-
+# Backend Address Pool Address (Simplified and working version)
 resource "azurerm_lb_backend_address_pool_address" "aks_vmss" {
   name                    = "${var.cluster_name}-vmss"
   backend_address_pool_id = azurerm_lb_backend_address_pool.aks_backend_pool.id
   virtual_network_id      = var.vnet_id
 
-  ip_address = data.azurerm_kubernetes_cluster.main.private_fqdn
+  # Use first available IP from your first private subnet
+  ip_address = cidrhost(var.private_subnet_cidrs[0], 10) # 10.0.16.10
 
   depends_on = [
     azurerm_lb.aks_custom_lb,
-    azurerm_lb_backend_address_pool.aks_backend_pool,
-    data.azurerm_kubernetes_cluster.main
+    azurerm_lb_backend_address_pool.aks_backend_pool
   ]
 }
