388b0c468fe0fdc6c2847eb90e63367229e649e2
pilotgab <captain.gab@protonmail.com>
2025-03-28 14:07:12 +0100

    update

--- aks-cluster/modules/vnet/main.tf
+++ aks-cluster/modules/vnet/main.tf
@@ -228,7 +228,7 @@ resource "azurerm_network_security_group" "private" {
 resource "azurerm_network_security_group" "aks_node_nsg" {
   name                = "aks-node-nsg"
   location            = data.azurerm_resource_group.this.location
-  resource_group_name = "${var.resource_group_name}-nrg"
+  resource_group_name = "${var.resource_group_name}"
 
   security_rule {
     name                       = "AllowLbHealthProbes"
